04073614cf8a611648712cc10e95bfe9d99628f2
Filipy <89490624+FilipyStudy@users.noreply.github.com>
2023-06-11 02:30:53 -0300

    Update TextRecognizer.cs

--- TextRecognizer.cs
+++ TextRecognizer.cs
@@ -6,10 +6,10 @@ namespace Csharp_Text_Recognizer
         static void Main(string[] args)
         {
             var ocr = new IronTesseract();
-            using (var input = new OcrInput(@"C:\Users\filip\Downloads\(1º) Lynn Beagheal - SQL Use a Cabeça!-Alta Ebooks (2008).pdf"))
+            using (var input = new OcrInput(@"Image-path"))
             {
                 var result = ocr.Read(input);
-                result.SaveAsTextFile(@"C:\Users\filip\OneDrive\Área de Trabalho\file.txt");
+                result.SaveAsTextFile(@"Output-path");
             }
         }
     }
